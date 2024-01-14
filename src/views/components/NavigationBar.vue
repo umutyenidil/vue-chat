84938a299b8d5b6d0d856c1f7537ff76ce216a97
Umut Yenidil <umut.yenidil9@gmail.com>
2024-01-14 12:34:49 +0300

    navigation bar guncellendi

---
+++ src/views/components/NavigationBar.vue
@@ -0,0 +1,94 @@
+<script setup>
+import TopNavigationBarItem from "@/views/components/NavigationBarItem.vue";
+import {useStore} from "vuex";
+import {computed, ref} from "vue";
+import DropdownButton from "@/views/components/DropdownButton.vue";
+import DropdownButtonItem from "@/views/components/DropdownButtonItem.vue";
+import {logout} from "@/services/firebase/auth/auth-service";
+import router from "@/router";
+
+const store = useStore();
+
+const user = computed(() => {
+  console.log(store.state.moduleAuth.user);
+  return store.state.moduleAuth.user;
+});
+
+const isVisibleMobileMenu = ref(false);
+const toggleMobileMenuVisibility = () => {
+  isVisibleMobileMenu.value = !isVisibleMobileMenu.value;
+};
+
+const onLogout = async () => {
+  try {
+    await logout();
+    await store.dispatch('moduleAuth/removeCurrentUser');
+    await router.push({name: 'welcome-page'});
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+
+</script>
+
+<template>
+  <nav class="px-[2rem] sm:px-[7.5rem] bg-slate-100 py-5 w-full flex justify-between items-center shadow h-min">
+    <TopNavigationBarItem title="Vue Chat" to="/"/>
+    <button @click="toggleMobileMenuVisibility"
+            class="block sm:hidden hover:bg-slate-200 transition duration-50 p-2 rounded-full text-center">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+           style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
+        <path d="M4 6h16v2H4zm4 5h12v2H8zm5 5h7v2h-7z"></path>
+      </svg>
+    </button>
+    <div class="hidden sm:block">
+      <div v-if="!user" class="flex justify-between gap-x-5">
+        <TopNavigationBarItem title="Register" to="/auth/register"/>
+        <TopNavigationBarItem title="Login" to="/auth/login"/>
+      </div>
+      <div v-else>
+        <DropdownButton :title="user.displayName">
+          <DropdownButtonItem @onPressed="console.log('test');">{{ user.email }}</DropdownButtonItem>
+          <DropdownButtonItem @onPressed="onLogout">
+            <p class="inline-flex items-center gap-x-2">
+              <i class='bx bx-log-out'></i>
+              <span>Logout</span>
+            </p>
+          </DropdownButtonItem>
+        </DropdownButton>
+      </div>
+    </div>
+  </nav>
+  <Transition name="slide-up">
+    <div v-if="isVisibleMobileMenu"
+         class="block sm:hidden absolute w-full h-min bg-slate-100 px-[2rem] py-5">
+      <ul class="flex flex-col gap-y-2">
+        <li>
+          <TopNavigationBarItem title="Register" to="/auth/register"/>
+        </li>
+        <li>
+          <TopNavigationBarItem title="Login" to="/auth/login"/>
+        </li>
+      </ul>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all 0.15s ease-out;
+}
+
+.slide-up-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.slide-up-leave-to {
+  opacity: 0;
+  transform: translateY(0px);
+}
+</style>
---
+++ src/views/layouts/NavigationBarLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavigationBar from "@/views/components/NavigationBar.vue";
+</script>
+
+<template>
+  <div class="bg-red-500 min-h-screen max-h-full">
+    <NavigationBar/>
+
+    <main class="bg-blue-500 max-h-full">
+      <router-view/>
+    </main>
+  </div>
+</template>

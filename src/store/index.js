e7a472cc738fe13206e6326460bd4843c3922212
Umut Yenidil <umut.yenidil9@gmail.com>
2024-02-03 13:21:38 +0300

    store index temizlendi

--- src/store/index.js
+++ src/store/index.js
@@ -6,33 +6,6 @@ import moduleRegisterPage from '@/store/modules/pages/register-page';
 import moduleChatRoomPage from '@/store/modules/pages/chat-room-page';
 
 export default createStore({
-    state: {
-        // modalState: {
-        //     show: false,
-        //     title: null,
-        //     message: null,
-        //     type: 'error',
-        // }
-    },
-    getters: {},
-    mutations: {
-        // setModalMessage(state, {type, title, message}) {
-        //     state.modalState.type = type;
-        //     state.modalState.title = title;
-        //     state.modalState.message = message;
-        // },
-        // showModal(state, {}) {
-        //     state.modalState.show = true;
-        // },
-        // closeModal(state,{}){
-        //     state.modalState.show = false;
-        // }
-    },
-    actions: {
-        // showModal(context){
-        //     context.commit('showModal', {});
-        // }
-    },
     modules: {
         moduleModal,
         moduleRegisterPage,
